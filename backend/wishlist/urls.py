a1c209251a90556f456b8ef84ae67e408d7f0b6a
theo <theophilus3031@gmail.com>
2026-06-09 19:36:59 +0300

    resolved product adding and login

--- backend/wishlist/urls.py
+++ backend/wishlist/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from .views import WishlistViewSet
 
 router = DefaultRouter()
-router.register('', WishlistViewSet)
+router.register('', WishlistViewSet, basename='wishlist')
 
 urlpatterns = router.urls
